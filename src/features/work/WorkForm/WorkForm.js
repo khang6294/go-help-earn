f793a878783816a892e13ad98b3ba5c179c55778
Khang Nguyen <ken.nguyen.bk62@gmail.com>
2019-02-04 23:32:25 +0700

    Define and handle states in WorkForm

--- src/features/work/WorkForm/WorkForm.js
+++ src/features/work/WorkForm/WorkForm.js
@@ -2,31 +2,56 @@ import React, { Component } from 'react'
 import {Segment,Form,Button} from 'semantic-ui-react'
 
 class WorkForm extends Component {
+
+    state = {
+        event: {
+            title: '',
+            date:'',
+            city:'',
+            venue:'',
+            postedBy:''
+        }
+    }
+
+    handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(this.state.event)
+    }
+
+    handleInputChange = (e) => {
+        const newEvent = {...this.state.event}
+        newEvent[e.target.name] = e.target.value;
+        this.setState({
+            event: newEvent
+        })
+    }
+
     render() {
         const {handleCloseForm} = this.props
+        const {event} = this.state
         return (
         <>
             <Segment>
-            <Form>
+            <Form onSubmit={this.handleSubmit}>
                 <Form.Field>
                     <label>Work Title</label>
-                    <input placeholder="Work Name" />
+                    <input name="title" value={event.title} onChange={this.handleInputChange} placeholder="Work Name" />
                 </Form.Field>
                 <Form.Field>
                     <label>Event Date</label>
-                    <input type="date" placeholder="Event Date" />
+                    <input name="date" value={event.date} onChange={this.handleInputChange} type="date" placeholder="Event Date" />
                 </Form.Field>
                 <Form.Field>
                     <label>City</label>
-                    <input placeholder="City event is taking place" />
+                    <input name="city" value={event.city} onChange={this.handleInputChange} placeholder="City work is taking place" />
                 </Form.Field>
                 <Form.Field>
                 <label>Venue</label>
-                <input placeholder="Enter the Venue of the event" />
+                <input name="venue" value={event.venue} onChange={this.handleInputChange} placeholder="Enter the Venue of the work" />
                 </Form.Field>
                 <Form.Field>
                     <label>Posted By</label>
-                    <input placeholder="Enter the name of person posting" />
+                    <input name="postedBy" value={event.postedBy} onChange={this.handleInputChange} placeholder="Enter the name of person posting" />
                 </Form.Field>
                 <Button positive type="submit">
                     Submit
